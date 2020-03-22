4102ff693f53f968c1740ded5f7201bc8133903f
oscarmeanwell <oscar.meanwell@gmail.com>
2020-03-22 15:06:02 +0000

    User login/signup completed

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,18 +2,31 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.math.BigInteger;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
 
 public class Welcome {
 
 	public JFrame frame;
 	public JTextField txtUser;
-	private JTextField txtPassword;
+	private JPasswordField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -69,31 +82,32 @@ public class Welcome {
 		frame.getContentPane().add(txtUser);
 		txtUser.setColumns(10);
 		
-		txtPassword = new JTextField();
-		txtPassword.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				if(txtPassword.getText().toString().equals("Password")) {
-					txtPassword.setText("");
-				}
-			}
-			@Override
-			public void focusLost(FocusEvent e) {
-				if(txtPassword.getText().toString().trim().equals("")) {
-					txtPassword.setText("Password");
-				}
-			}
-		});
-		
-		txtPassword.setText("Password");
-		txtPassword.setColumns(10);
-		txtPassword.setBounds(164, 121, 124, 19);
-		frame.getContentPane().add(txtPassword);
-		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("TEST");
+				String usr = txtUser.getText().toString();
+				String pwd = String.valueOf(txtPassword.getPassword());
+				MessageDigest md5 = null;
+				try {
+					md5 = MessageDigest.getInstance("MD5");
+				} catch (NoSuchAlgorithmException e1) {
+					e1.printStackTrace();
+				}
+				md5.update(StandardCharsets.UTF_8.encode(pwd));
+				String pwdHashed = String.format("%032x", new BigInteger(1, md5.digest()));
+				String toPass = "http://oscarmeanwell.me:3001/retrieve?usr=" + usr + "&pwd=" + pwdHashed;
+				try {
+					JSONObject json = new JSONObject(IOUtils.toString(new URL(toPass), Charset.forName("UTF-8")));
+					if(Integer.parseInt(json.get("status").toString()) == 1) {
+						System.out.println("LOGIN");
+					}
+					else {
+						JOptionPane.showMessageDialog(frame, "Authentification error", "InfoBox: Registration", JOptionPane.INFORMATION_MESSAGE);
+					}
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
 		btnLogin.setBounds(164, 152, 124, 25);
@@ -110,6 +124,25 @@ public class Welcome {
 		});
 		btnRegister.setBounds(12, 235, 114, 25);
 		frame.getContentPane().add(btnRegister);
+		
+		txtPassword = new JPasswordField();
+		txtPassword.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(String.valueOf(txtPassword.getPassword()).equals("Password")) {
+					txtPassword.setText("");
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(String.valueOf(txtPassword.getPassword()).equals("")) {
+					txtPassword.setText("Password");
+				}
+			}
+		});
+		txtPassword.setBounds(164, 121, 124, 19);
+		txtPassword.setText("Password");
+		frame.getContentPane().add(txtPassword);
 		
 	}
 }
