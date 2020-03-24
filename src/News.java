865cea3191ac9976aea11937ea74d2fc4adf2403
oscarmeanwell <oscar.meanwell@gmail.com>
2020-03-24 18:30:56 +0000

    Dead

--- src/News.java
+++ src/News.java
@@ -32,6 +32,13 @@ import java.awt.event.ActionEvent;
 public class News {
 
 	public JFrame frame;
+	JLabel lblLINK = new JLabel("");
+	JTextArea txtArea = new JTextArea();
+	JLabel lblLink = new JLabel("Link:");
+	JLabel lblPublished = new JLabel("Published");
+	JLabel lblTitle = new JLabel("Title");
+	JList lstNews = null;
+	JLabel lblClickATitle = new JLabel("NEWS: Click a title to see the details");
 	HashMap<String, JSONObject> main = new HashMap<>();
 	public static void main(String[] args) {
 		News x = new News();
@@ -44,29 +51,21 @@ public class News {
 				frame = new JFrame();
 				frame.setBounds(100, 100, 640, 459);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				//frame.getContentPane().setLayout(null);
-				
-				
 				ArrayList<String> newsTitles = new ArrayList<>();
-				
 				try {
 					JSONArray json = new JSONArray(IOUtils.toString(new URL("http://oscarmeanwell.me/stockNews.php"), Charset.forName("UTF-8")));
 					
 					for(int i = 0; i < json.length(); i++) {
 						JSONObject tmp = json.getJSONObject(i);
 						main.put(tmp.get("title").toString(), tmp);
-						
 					}
 				
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				frame.getContentPane().setLayout(null);
-				//frame.getContentPane().setLayout(null);
-				//frame.getContentPane().setLayout(null);
-				//frame.getContentPane().setLayout(null);
 				
-				JList lstNews = new JList(main.keySet().toArray());
+				lstNews = new JList(main.keySet().toArray());
 				
 				//frame.getContentPane().add(lstNews);
 				JScrollPane scrollP = new JScrollPane(lstNews);
@@ -74,27 +73,29 @@ public class News {
 				lstNews.setBounds(0, 45, 640, 111);
 				frame.getContentPane().add(scrollP);
 				
-				JLabel lblClickATitle = new JLabel("NEWS: Click a title to see the details");
+				
 				lblClickATitle.setBounds(12, 12, 376, 15);
 				frame.getContentPane().add(lblClickATitle);
 				
-				JLabel lblTitle = new JLabel("New label");
+				
 				lblTitle.setBounds(12, 184, 598, 15);
 				frame.getContentPane().add(lblTitle);
 				
-				JLabel lblPublished = new JLabel("Published");
+				
 				lblPublished.setBounds(12, 211, 574, 15);
 				frame.getContentPane().add(lblPublished);
 				
-				JLabel lblLink = new JLabel("Link:");
+				
 				lblLink.setBounds(12, 238, 66, 15);
 				frame.getContentPane().add(lblLink);
 				
-				JTextArea txtArea = new JTextArea();
+				
 				txtArea.setBounds(12, 265, 616, 115);
 				frame.getContentPane().add(txtArea);
+				lstNews.setSelectedIndex(0);
+				updateLBLS();
+			
 				
-				JLabel lblLINK = new JLabel("");
 				lblLINK.addMouseListener(new MouseAdapter() {
 					@Override
 					public void mouseClicked(MouseEvent e) {
@@ -122,26 +123,11 @@ public class News {
 				frame.getContentPane().add(btnDone);
 				frame.setDefaultCloseOperation(0);
 				frame.setVisible(true);
+				
 				lstNews.addListSelectionListener(new ListSelectionListener() {
 					public void valueChanged(ListSelectionEvent arg0) {
 						if (!arg0.getValueIsAdjusting()) {//This line prevents double events
-							String article = lstNews.getSelectedValue().toString();
-							lblTitle.setText("Title: " + article);
-							SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd hh:mm");
-							String p1 = main.get(article).getString("publishedAt").substring(0, main.get(article).getString("publishedAt").length()-1);
-							p1 = p1.replace('T', ' ');
-							Date formattedD = null;
-							try {
-								formattedD = format1.parse(p1);
-								System.out.println(formattedD);
-							} catch (Exception e) {
-								e.printStackTrace();
-							} 
-							lblPublished.setText("Published: " + formattedD);
-							txtArea.setText(main.get(article).getString("description"));
-							txtArea.setLineWrap(true);
-							lblLINK.setText(main.get(article).getString("url"));
-							lblLINK.setForeground(Color.BLUE);
+							updateLBLS();
 					    }
 						
 					}
@@ -149,6 +135,27 @@ public class News {
 			}
 		});
 	}
+	
+	public void updateLBLS() {
+		String article = lstNews.getSelectedValue().toString();
+		lblTitle.setText("Title: " + article);
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		String p1 = main.get(article).getString("publishedAt").substring(0, main.get(article).getString("publishedAt").length()-1);
+		p1 = p1.replace('T', ' ');
+		Date formattedD = null;
+		try {
+			formattedD = format1.parse(p1);
+			System.out.println(formattedD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		lblPublished.setText("Published: " + formattedD);
+		txtArea.setText(main.get(article).getString("description"));
+		txtArea.setLineWrap(true);
+		lblLINK.setText(main.get(article).getString("url"));
+		lblLINK.setForeground(Color.BLUE);
+	}
 
 
+	
 }
